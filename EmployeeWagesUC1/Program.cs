9014773dc394609437d39404196a27e73199e330
ShivaniDivekar25 <shivanidivekar2@gmail.com>
2022-12-15 17:53:06 +0530

    Added Welcome Employee Wages Computation

---
+++ EmployeeWagesUC1/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace EmployeeWagesUC1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to employee wages computation program!");
+        }
+    }
+}
