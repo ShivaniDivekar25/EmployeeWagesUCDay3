0ce78672d7333d82a8934ae3d1ea7f543e66811b
ShivaniDivekar25 <shivanidivekar2@gmail.com>
2022-12-15 19:04:58 +0530

    Added Employee Wages UC3 program

--- EmployeeWagesUC1/Program.cs
+++ EmployeeWagesUC1/Program.cs
@@ -4,7 +4,7 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Enter 0 for Welcome message program\nEnter 1 for UC1\nEnter 2 for UC2");
+            Console.WriteLine("Enter 0 for Welcome message program\nEnter 1 for UC1\nEnter 2 for UC2\nEnter 3 for UC3");
             int option = Convert.ToInt32(Console.ReadLine());
             switch (option)
             {
@@ -20,6 +20,11 @@
                     Random random1 = new Random();
                     int empCheck1 = random1.Next(0, 2);
                     EmployeeWagesUC2.EmployeeUC2(empCheck1);
+                    break;
+                case 3:
+                    Random random2 = new Random();
+                    int empCheck2 = random2.Next(0, 3);
+                    EmployeeWagesUC3.EmployeeUC3(empCheck2);
                     break;
                 default:
                     Console.WriteLine("Please enter right option");
