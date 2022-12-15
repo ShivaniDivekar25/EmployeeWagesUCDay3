24f90fd8d16c002cc1a8a70ed2605ef4fecd9a2c
ShivaniDivekar25 <shivanidivekar2@gmail.com>
2022-12-15 17:58:22 +0530

    Added Employee Wages UC1 program

--- EmployeeWagesUC1/Program.cs
+++ EmployeeWagesUC1/Program.cs
@@ -5,6 +5,9 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to employee wages computation program!");
+            Random random = new Random();
+            int empCheck = random.Next(0, 2);
+            EmployeeWagesUC1.EmployeeUC1(empCheck);
         }
     }
 }
